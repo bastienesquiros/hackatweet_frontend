d7cc3ff63d4b625658be9dbb47a1d9c094c2d62e
Bastien <Udshoft@gmail.com>
2023-02-09 17:41:56 +0100

    ajouts des modules

--- components/Login.js
+++ components/Login.js
@@ -1,29 +1,41 @@
-import Image from "next/image";
-import styles from "../styles/Login.module.css";
+import Image from 'next/image';
+import styles from '../styles/Login.module.css';
+
+import { Button, Modal } from 'antd';
+import { useState } from 'react';
+import Signup from './Signup';
+import Signin from './Signin';
 
 function Login() {
-	// const [signUp, setSignUp] = useState("");
-	// const [signIn, setSignIn] = useState("");
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const showModal = () => {
+		setIsModalOpen(true);
+	};
+	const handleOk = () => {
+		setIsModalOpen(false);
+	};
+	const handleCancel = () => {
+		setIsModalOpen(false);
+	};
+
+	const [isModalOpen2, setIsModalOpen2] = useState(false);
+	const showModal2 = () => {
+		setIsModalOpen2(true);
+	};
+	const handleOk2 = () => {
+		setIsModalOpen2(false);
+	};
+	const handleCancel2 = () => {
+		setIsModalOpen2(false);
+	};
 
 	return (
 		<div className={styles.main}>
 			<div className={styles.left}>
-				<Image
-					className={styles.image}
-					src="/logo.png"
-					alt="logo"
-					width={250}
-					height={250}
-				/>
+				<Image className={styles.image} src="/logo.png" alt="logo" width={250} height={250} />
 			</div>
 			<div className={styles.right}>
-				<Image
-					className={styles.image2}
-					src="/logo.png"
-					alt="logo"
-					width={250}
-					height={250}
-				/>
+				<Image className={styles.image2} src="/logo.png" alt="logo" width={250} height={250} />
 				<div className={styles.sentence1}>
 					<h1 className={styles.h1}>
 						See what's<br></br>happening
@@ -33,13 +45,47 @@ function Login() {
 					<h2 className={styles.h2}>Join Hackatweet today.</h2>
 				</div>
 				<div className={styles.button}>
-					<button className={styles.signup}>Sign Up</button>
+					<button onClick={showModal2} className={styles.signup}>
+						Sign Up
+					</button>
+					<Modal
+						maskStyle={false}
+						style={{ backgroundColor: '#27323E', border: '0px', borderRadius: '50%' }}
+						bodyStyle={{ borderRadius: '50%', backgroundColor: 'white', padding: '0px', height: '100%' }}
+						footer={null}
+						maskClosable={true}
+						width={'30%'}
+						height={'35%'}
+						open={isModalOpen2}
+						className="modalStyle"
+						onOk={handleOk2}
+						onCancel={handleCancel2}
+					>
+						<Signup />
+					</Modal>
 				</div>
 				<div className={styles.sentence4}>
 					<h4>Already have an account?</h4>
 				</div>
 				<div className={styles.button}>
-					<button className={styles.signin}>Sign In</button>
+					<button onClick={showModal} className={styles.signin}>
+						Sign In
+					</button>
+					<Modal
+						maskStyle={false}
+						style={{ backgroundColor: '#27323E', border: '0px', borderRadius: '50%' }}
+						bodyStyle={{ borderRadius: '50%', backgroundColor: 'white', padding: '0px', height: '100%' }}
+						footer={null}
+						maskClosable={true}
+						width={'30%'}
+						height={'35%'}
+						open={isModalOpen}
+						className="modalStyle"
+						onOk={handleOk}
+						onCancel={handleCancel}
+					>
+						<Signin />
+					</Modal>
 				</div>
 			</div>
 		</div>
--- components/signUp.js
+++ components/signUp.js
@@ -1,39 +1,67 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import styles from '../styles/SignUp.module.css';
 import Image from 'next/image';
 
-function SignUp(){
-    const [firstname,setFirstName]=useState('')
-    const [username,setUsername]=useState('')
-    const [password,setPassword]=useState('')
+function SignUp() {
+	const [firstname, setFirstName] = useState('');
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
 
-    const signupHandleClick=()=>{
-        fetch('http://localhost:3000/users/signup',{
-            method:'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ firstname: firstname,username:username, password: password }),
-            }).then(data=>data.json()).then(res=>{
-                if(res.result){
-                    window.location.href = '/homeTweets';
-                }
-            })
-    }
-    return(
-    <div className={styles.container}>
-        <div className={styles.signUpCard}>
-            <div className={styles.header}>
-                <h1 id={styles.cross}>x</h1>
-            </div>
-            <Image src="/logo.png" width={30} height={30} />
-            <h2 id={styles.h2}>Create your hackatweet account</h2>
-            <input placeholder="Firstname" onChange={(e)=>{setFirstName(e.target.value)}} value={firstname} className={styles.inputs}/>
-            <input placeholder="Username" onChange={(e)=>{setUsername(e.target.value)}} value={username} className={styles.inputs}/>
-            <input type="password" placeholder="Password" onChange={(e)=>{setPassword(e.target.value)}} value={password}  className={styles.inputs}/>
-            <button onClick={()=>{signupHandleClick()}} className={styles.signupButton} >Sign Up</button>
-        </div>
-    </div>
-    )
-    
+	const signupHandleClick = () => {
+		fetch('http://localhost:3000/users/signup', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ firstname: firstname, username: username, password: password }),
+		})
+			.then((data) => data.json())
+			.then((res) => {
+				if (res.result) {
+					window.location.href = '/homeTweets';
+				}
+			});
+	};
+	return (
+		<div className={styles.container}>
+			<div className={styles.signUpCard}>
+				<div className={styles.header}></div>
+				<Image src="/logo.png" width={35} height={35} />
+				<h2 id={styles.h2}>Create your hackatweet account</h2>
+				<input
+					placeholder="Firstname"
+					onChange={(e) => {
+						setFirstName(e.target.value);
+					}}
+					value={firstname}
+					className={styles.inputs}
+				/>
+				<input
+					placeholder="Username"
+					onChange={(e) => {
+						setUsername(e.target.value);
+					}}
+					value={username}
+					className={styles.inputs}
+				/>
+				<input
+					type="password"
+					placeholder="Password"
+					onChange={(e) => {
+						setPassword(e.target.value);
+					}}
+					value={password}
+					className={styles.inputs}
+				/>
+				<button
+					onClick={() => {
+						signupHandleClick();
+					}}
+					className={styles.signupButton}
+				>
+					Sign Up
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default SignUp;
