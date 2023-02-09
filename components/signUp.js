780f25d06a3fe5038547bf7453a25bcf761266a7
theostone62 <theostone62@gmail.com>
2023-02-09 14:47:42 +0100

    signup Hometweet

--- components/signUp.js
+++ components/signUp.js
@@ -1,6 +1,22 @@
+import { useState } from 'react'
 import styles from '../styles/SignUp.module.css'
 
 function SignUp(){
+    const [firstname,setFirstName]=useState('')
+    const [username,setUsername]=useState('')
+    const [password,setPassword]=useState('')
+
+    const signupHandleClick=()=>{
+        fetch('http://localhost:3000/users/signup',{
+            method:'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ firstname: firstname,username:username, password: password }),
+            }).then(data=>data.json()).then(res=>{
+                if(res.result){
+                    window.location.href = '/homeTweets';
+                }
+            })
+    }
     return(
     <div className={styles.container}>
         <div className={styles.signUpCard}>
@@ -9,10 +25,10 @@ function SignUp(){
             </div>
             <h2 id={styles.h2}>A</h2>
             <h2 id={styles.h2}>Create your hackatweet account</h2>
-            <input className={styles.inputs}></input>
-            <input className={styles.inputs}></input>
-            <input className={styles.inputs}></input>
-            <button className={styles.signupButton}>Sign Up</button>
+            <input onChange={(e)=>{setFirstName(e.target.value)}} value={firstname} className={styles.inputs}/>
+            <input onChange={(e)=>{setUsername(e.target.value)}} value={username} className={styles.inputs}/>
+            <input onChange={(e)=>{setPassword(e.target.value)}} value={password}  className={styles.inputs} type="password"/>
+            <button onClick={()=>{signupHandleClick()}} className={styles.signupButton} >Sign Up</button>
         </div>
     </div>
     )
---
+++ pages/homeTweets.js
@@ -0,0 +1,7 @@
+import HomeTweet from '../components/HomeTweet';
+
+function HomeTweets() {
+  return <HomeTweet />;
+}
+
+export default HomeTweets;
