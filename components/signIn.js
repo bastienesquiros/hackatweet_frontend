f70f0251e7f8177ef0eb818a17f748febb633381
Bastien <Udshoft@gmail.com>
2023-02-09 14:12:09 +0100

    Ajout des components Sign

--- components/signIn.js
+++ components/signIn.js
@@ -1,7 +1,7 @@
-import styles from '../styles/SignUp.module.css';
 import Image from 'next/image';
+import styles from '../styles/SignIn.module.css';
 
-function SignUp() {
+function SignIn() {
 	return (
 		<div className={styles.container}>
 			<div className={styles.signUpCard}>
@@ -9,14 +9,13 @@ function SignUp() {
 					<h1 id={styles.cross}>x</h1>
 				</div>
 				<Image src="/logo.png" width={30} height={30} />
-				<h2 id={styles.h2}>Create your hackatweet account</h2>
-				<input placeholder="Firstname" className={styles.inputs}></input>
+				<h2 id={styles.h2}>Connect to Hackatweet</h2>
 				<input placeholder="Username" className={styles.inputs}></input>
 				<input type="password" placeholder="Password" className={styles.inputs}></input>
-				<button className={styles.signupButton}>Sign Up</button>
+				<button className={styles.signupButton}>Sign In</button>
 			</div>
 		</div>
 	);
 }
 
-export default SignUp;
+export default SignIn;
