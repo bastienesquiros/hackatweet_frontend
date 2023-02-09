0890f6f87dc0a1a32d45c5a22fd6410e6941317b
vivenot_tom@hotmail.fr <vivenot_tom@hotmail.fr>
2023-02-09 14:40:30 +0100

    design login

---
+++ components/Login.js
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import styles from "../styles/Login.module.css";
+
+function Login() {
+	// const [signUp, setSignUp] = useState("");
+	// const [signIn, setSignIn] = useState("");
+
+	return (
+		<div className={styles.main}>
+			<div className={styles.left}>
+				<Image
+					className={styles.image}
+					src="/logo.png"
+					alt="logo"
+					width={250}
+					height={250}
+				/>
+			</div>
+			<div className={styles.right}>
+				<Image
+					className={styles.image2}
+					src="/logo.png"
+					alt="logo"
+					width={250}
+					height={250}
+				/>
+				<div className={styles.sentence1}>
+					<h1>See what's</h1>
+				</div>
+				<div className={styles.sentence2}>
+					<h1>happening</h1>
+				</div>
+				<div className={styles.sentence3}>
+					<h3>Join Hackatweet today.</h3>
+				</div>
+				<div className={styles.button}>
+					<button className={styles.signup}>Sign Up</button>
+				</div>
+				<div className={styles.sentence4}>
+					<h4>Already have an account?</h4>
+				</div>
+				<div className={styles.button}>
+					<button className={styles.signin}>Sign In</button>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Login;
