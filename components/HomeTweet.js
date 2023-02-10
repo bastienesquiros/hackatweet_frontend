4be8c6586802a6ce884d15176b712b37368a1ef1
vivenot_tom@hotmail.fr <vivenot_tom@hotmail.fr>
2023-02-10 10:55:30 +0100

    heart button

--- components/HomeTweet.js
+++ components/HomeTweet.js
@@ -1,7 +1,38 @@
 import Image from "next/image";
 import styles from "../styles/HomeTweet.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 function HomeTweet() {
+	const [like, setLike] = useState();
+
+	const personalLike = [];
+	let style = {};
+	const stylered = { color: "red" };
+	const styleblack = { color: "" };
+	for (let i = 0; i < 1; i++) {
+		if (like) {
+			style = stylered;
+		} else {
+			style = styleblack;
+		}
+
+		personalLike.push(
+			<FontAwesomeIcon
+				key={i}
+				icon={faHeart}
+				onClick={() => setLike(like === false)}
+				style={style}
+				className="like"
+			/>
+		);
+	}
+
+	const returnToLoginPage = () => {
+		window.location.href = "/";
+	};
+
 	return (
 		<div className={styles.main}>
 			<div className={styles.left}>
@@ -25,7 +56,14 @@ function HomeTweet() {
 					<div className={styles.down2}>
 						<h3 className={styles.name}>John</h3>
 						<h3 className={styles.name2}>@JohnCena</h3>
-						<button className={styles.button1}>Logout</button>
+						<button
+							className={styles.button1}
+							onClick={() => {
+								returnToLoginPage();
+							}}
+						>
+							Logout
+						</button>
 					</div>
 				</div>
 			</div>
@@ -41,6 +79,35 @@ function HomeTweet() {
 						/>
 					</div>
 					<button className={styles.button2}>Tweet</button>
+				</div>
+				<div className={styles.middle3}>
+					<div className={styles.middle4}>
+						<Image
+							className={styles.image2}
+							src="/oeuf.png"
+							alt="oeuf"
+							width={250}
+							height={250}
+						/>
+						<h3 className={styles.name3}>Antoine</h3>
+						<h3 className={styles.name4}>@AntoineLeProf</h3>
+						<h3 className={styles.moment}> - Now</h3>
+					</div>
+					<div className={styles.middle5}>
+						<p className={styles.comment}>
+							Welcome to{" "}
+							<span className={styles.hashtag}>#hackatweet </span>
+							guys 😎
+						</p>
+					</div>
+					<div className={styles.like}>
+						<span className={styles.like2}>
+							{personalLike} ({like})
+						</span>
+						<span className={styles.trash}>
+							<FontAwesomeIcon icon={faTrashCan} />
+						</span>
+					</div>
 				</div>
 			</div>
 			<div className={styles.right}>
